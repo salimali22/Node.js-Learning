0827393f991834f39b617c187399becde534e476
salimali22 <57323200+salimali22@users.noreply.github.com>
2022-11-16 00:23:22 +0500

    Navigation using Node.js URL module
    Navigation Between two pages is done using Node.js
    URL module is used to get the file name from the ur;

--- http.js
+++ http.js
@@ -1,17 +1,36 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const port = 8080;
-const host = "127.0.0.1"
+const host = "localhost"
+// console.log(fs.readFileSync("./index.html","UTF-8"));
+const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    const filename = url.parse(req.url).pathname;
+    console.log(filename);
+    fs.readFile("." + filename, "UTF-8", (error, file) => {
+        if (error) {
+            res.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+            res.end(`<h1>Not Found</h1>`);
+        }
+        else {
+            // console.log(file);
+            res.writeHead(200, { "Content-Type": 'text/html' });
+            // res.write(`<script>alert('hello');</script>`);
+            res.write(file);
+        }
 
-const server = http.createServer((req,res)=>{
-console.log(req.url);
-res.writeHead(200,{"Content-Type":'text/html'})
-res.write("<h4>This is me</h4>");
-res.write(`Request URL ${req.url}`)
-res.end();
+        // res.write("<h1>Not working</h1>");
+        // res.write(`Request URL ${req.url}`) 
+        res.end();
+    })
+
+
 })
 
-server.listen(port,host,()=>{
+server.listen(port, host, () => {
     console.log("Ready");
 })
